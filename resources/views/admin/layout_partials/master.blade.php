20ca9b3b26a3e1c2e7685b25f0b59f59439befb2
riymuh <mriyadh103@gmail.com>
2020-07-06 11:21:15 +0700

    fix

--- resources/views/admin/layout_partials/master.blade.php
+++ resources/views/admin/layout_partials/master.blade.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
         <!-- Google Font: Source Sans Pro -->
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+        <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
         <style>
                 #karyawan-table_paginate a{
                     cursor:pointer;
